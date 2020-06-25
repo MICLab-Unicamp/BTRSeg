3c6c4af92586b6a9e1a2774e51d709c4fad6daa4
dscarmo <carmodiedre@outlook.com>
2020-06-25 15:14:13 -0300

    update dlpt release

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
 git clone https://github.com/MICLab-Unicamp/BTRSeg
 cp -r BTRSeg/* .
 rm -rfd BTRSeg
 mv data.zip data
+python3 -m pip install dlpt_installer/DLPT-0.0.1-py3-none-any.whl
 jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root --no-browser
